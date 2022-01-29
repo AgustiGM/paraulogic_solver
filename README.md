6ca603e2a428368e9ff83ebf4441ce4a74828566
AgustiGM <53912168+AgustiGM@users.noreply.github.com>
2022-01-29 20:28:04 +0100

    Minor edits.

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
-# Paraulògic solver
+# Solucionari del Paraulògic
 
-Small program that, given the letters for the day, generates a list of words that fit the criteria.
+Petit programa que, donades les lletres del dia, genera una llista amb paraules que compleixen els requisits.
+
+De moment aquesta versió inicial és força limitada, però hi aniré afegint funcions.
+
+Guia 
+1. Afegeix les lletres del dia al quadre de text. La primera lletra ha de ser la del mig.
+2. Prem "Genera" perquè les paraules que compleixen els requisits apareguin al camp de text de sota.
--- gui/mainWindow.py
+++ gui/mainWindow.py
@@ -12,12 +12,16 @@ class MainWindow:
         self.window.title(string="Solucionari")
         self.window.minsize(width=500,height=600)
         self.frame = Frame(self.window)
+        self.camp_label = Label(self.frame,text="Introdueix les lletres (primer l'obligatòria):")
+        self.camp_label.grid(row=0, column=0)
         self.camp_lletres = Entry(self.frame, width=20)
-        self.camp_lletres.grid(row=0, column=0)
+        self.camp_lletres.grid(row=0, column=1)
         self.genera_boto = Button(self.frame, text="Genera", command=self.onclick)
-        self.genera_boto.grid(row=0, column=1)
+        self.genera_boto.grid(row=0, column=2)
+        self.sol_label = Label(self.frame, text="Paraules del dia:")
+        self.sol_label.grid(row=1, column=0)
         self.text = scrolledtext.ScrolledText(self.frame, width=50, height=30)
-        self.text.grid(row=1, column=0, columnspan=2)
+        self.text.grid(row=2, column=0, columnspan=2)
         self.frame.pack(fill=tkinter.BOTH, side=tkinter.LEFT)
 
     def onclick(self):
@@ -29,7 +33,7 @@ class MainWindow:
         p = Processor()
         sol, tutis = p.generate_solution(letters)
         label = Label(self.frame,text="Paraules: "+str(len(sol)+len(tutis)))
-        label.grid(row=2, column=1)
+        label.grid(row=3, column=1)
         self.text.insert(tkinter.INSERT, chars="Tutis:\n")
         for word in tutis:
             self.text.insert(tkinter.INSERT, chars=word)
